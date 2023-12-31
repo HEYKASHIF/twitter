149a9a685f2dc8f3fd4e23903c1e8e4779f7b4b4
HEYKASHIF <kashifmohd269@gmail.com>
2023-12-31 21:22:58 +0530

    commit 4

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,4 +1,5 @@
 class AppwriteConstants {
   static const String databaseId = '65918bf07376eb796692';
   static const String projectId = '65907e1f1efecdc046b6';
+  static const String endPoint = 'http://localhost:80/v1';
 }
