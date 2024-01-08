7ee7cce2c894f8f03b7469d7f25b37faf3dec5c5
HEYKASHIF <kashifmohd269@gmail.com>
2024-01-08 23:25:38 +0530

    commit 20

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       title: 'twitter',
       theme: AppTheme.theme,
       home: const SignUpView(),
